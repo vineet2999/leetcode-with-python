93359d6f4a93ebc39fb90268041de301b109a125
vineet <kvineetreddy95@gmail.com>
2024-04-17 20:45:26 -0500

    heap solution

--- arrays/kth_largest_in_array.py
+++ arrays/kth_largest_in_array.py
@@ -9,10 +9,10 @@ Example 1:
 Input: nums = [3,2,1,5,6,4], k = 2
 Output: 5
 """
+import heapq
 
 
 # Solution:
-
 def k_th_largest_element(num: list[int], k: int) -> int:
     # sort the list in reverse order, return the kth element in the list
     if not num:
@@ -20,6 +20,26 @@ def k_th_largest_element(num: list[int], k: int) -> int:
 
     num.sort(reverse=True)
     return num[k - 1]
+
+
+# heap solution
+
+def heap_solution(nums: list[int], k: int) -> int:
+    heap = []
+    # Push the first k elements into the heap
+    for num in nums[:k]:
+        heapq.heappush(heap, num)
+
+    # For each element in nums starting from k,
+    # if the element is greater than the smallest element in the heap,
+    # pop the smallest element from the heap and push the current element
+    for num in nums[k:]:
+        if num > heap[0]:
+            heapq.heappop(heap[0])
+            heapq.heappush(heap, num)
+
+    # The root of the heap will be the kth largest element
+    return heap[0]
 
 
 if __name__ == '__main__':
